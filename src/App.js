53b2d16e89a52639578c7f34c43578e4c52746d1
Anna <anna.svrva@gmail.com>
2022-10-10 18:03:40 +0300

    changed default city to Kharkiv

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Milan" />
+        <Weather defaultCity="Kharkiv" />
         <footer>
           This project was coded by{" "}
           <a
